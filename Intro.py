c9f0fd3d6f0955af1e48672daec7608b7d99e6d9
🧐 ᴛᴜꜱʜᴀʀ-ᴘᴇʀꜱᴘᴇᴄᴛɪᴠᴇ <61559891+TUSHAR-PERSPECTIVE@users.noreply.github.com>
2024-06-13 03:15:51 +0530

    commit

--- Intro.py
+++ Intro.py
@@ -9,6 +9,6 @@ class Me:
     self.interests = "Open Source"
 
 def introduce(self):
-  print(f"Hi, I'm ({self.name}), {self.age} year old ..... ended up here.")
+  print(f"Hi I'm, {self.name}, {self.age} year old {self.profession} & freelancer from {self.country}. I've taken coding seriously since {self.coding_start_year} and have been freelancing since {self.freelancing_start_year}. Recently, however, I've grown a knack for giving back to community. I like contributing to {self.interests} with {Ink of Determaintion} . which is probably the reason why you've ended up here.")
 
 Me().introduce()
